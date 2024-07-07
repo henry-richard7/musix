3ea1e5ae0852be86c2de14a4e8bfc922ceadd97e
Henry-Richard7 <henry.richard.2k@gmail.com>
2024-07-07 21:20:42 +0530

    Some Changes

--- lib/pages/album_details_page.dart
+++ lib/pages/album_details_page.dart
@@ -54,86 +54,92 @@ class _AlbumDetailsPageState extends State<AlbumDetailsPage> {
               ? Column(
                   crossAxisAlignment: CrossAxisAlignment.start,
                   children: [
-                    Wrap(
-                      children: [
-                        Image.network(
-                          albumDetailsResponse['image']
-                              .toString()
-                              .replaceAll("150x150", "500x500"),
-                          filterQuality: FilterQuality.high,
-                          height: 260,
-                          fit: BoxFit.cover,
-                        ),
-                        Padding(
-                          padding: const EdgeInsets.all(8.0),
-                          child: Column(
-                            crossAxisAlignment: CrossAxisAlignment.start,
-                            children: [
-                              Text(
-                                albumDetailsResponse['title'],
-                                style: const TextStyle(
-                                  fontSize: 18,
-                                  fontWeight: FontWeight.bold,
-                                ),
-                              ),
-                              Text(
-                                "Released Date: ${albumDetailsResponse['release_date']}",
-                                style: const TextStyle(
-                                  fontSize: 16,
-                                ),
-                              ),
-                              Text(
-                                "Primary Artists: ${albumDetailsResponse['primary_artists']}",
-                                style: const TextStyle(
-                                  fontSize: 16,
-                                ),
-                              ),
-                            ],
-                          ),
-                        ),
-                      ],
-                    ),
-                    Expanded(
-                      child: Padding(
-                        padding: const EdgeInsets.all(8.0),
-                        child: ListView.builder(
-                            itemCount: songs.length,
-                            scrollDirection: Axis.vertical,
-                            itemBuilder: (context, index) {
-                              return Card(
-                                child: ListTile(
-                                  title: Text(
-                                    HtmlUnescape().convert(
-                                      songs[index]['song'].toString(),
-                                    ),
-                                    style: const TextStyle(
-                                        fontSize: 16,
-                                        fontWeight: FontWeight.bold),
-                                  ),
-                                  subtitle: Text(
-                                    HtmlUnescape().convert(
-                                      songs[index]['primary_artists']
-                                          .toString(),
-                                    ),
-                                    style: const TextStyle(fontSize: 14),
-                                  ),
-                                  leading: Image.network(
-                                    songs[index]['image']
-                                        .toString()
-                                        .replaceAll("150x150", "500x500"),
-                                    filterQuality: FilterQuality.high,
-                                  ),
-                                ),
-                              );
-                            }),
-                      ),
-                    )
+                    albumDetailWidgets(),
+                    albumSongs(),
                   ],
                 )
               : const Center(
                   child: CircularProgressIndicator(),
                 ),
         ),
+      ),
+    );
+  }
+
+  Wrap albumDetailWidgets() {
+    return Wrap(
+      children: [
+        Image.network(
+          albumDetailsResponse['image']
+              .toString()
+              .replaceAll("150x150", "500x500"),
+          filterQuality: FilterQuality.high,
+          height: 260,
+          fit: BoxFit.cover,
+        ),
+        Padding(
+          padding: const EdgeInsets.all(8.0),
+          child: Column(
+            crossAxisAlignment: CrossAxisAlignment.start,
+            children: [
+              Text(
+                albumDetailsResponse['title'],
+                style: const TextStyle(
+                  fontSize: 18,
+                  fontWeight: FontWeight.bold,
+                ),
+              ),
+              Text(
+                "Released Date: ${albumDetailsResponse['release_date']}",
+                style: const TextStyle(
+                  fontSize: 16,
+                ),
+              ),
+              Text(
+                "Primary Artists: ${albumDetailsResponse['primary_artists']}",
+                style: const TextStyle(
+                  fontSize: 16,
+                ),
+              ),
+            ],
+          ),
+        ),
+      ],
+    );
+  }
+
+  Expanded albumSongs() {
+    return Expanded(
+      child: Padding(
+        padding: const EdgeInsets.all(8.0),
+        child: ListView.builder(
+            itemCount: songs.length,
+            scrollDirection: Axis.vertical,
+            itemBuilder: (context, index) {
+              return Card(
+                child: ListTile(
+                  title: Text(
+                    HtmlUnescape().convert(
+                      songs[index]['song'].toString(),
+                    ),
+                    style: const TextStyle(
+                        fontSize: 16, fontWeight: FontWeight.bold),
+                  ),
+                  subtitle: Text(
+                    HtmlUnescape().convert(
+                      songs[index]['primary_artists'].toString(),
+                    ),
+                    style: const TextStyle(fontSize: 14),
+                  ),
+                  leading: Image.network(
+                    songs[index]['image']
+                        .toString()
+                        .replaceAll("150x150", "500x500"),
+                    filterQuality: FilterQuality.high,
+                  ),
+                ),
+              );
+            }),
       ),
     );
   }
